88fb6c617d439010d8e7db4634a83f51df7f5787
Ghazanfar Naqvi <1874327+gnaqvi@users.noreply.github.com>
2018-10-07 08:40:46 -0400

    Weekend cleanup!

--- .config/polybar/scripts/now_playing.py
+++ .config/polybar/scripts/now_playing.py
@@ -2,9 +2,21 @@
 
 from gi.repository import Playerctl, GLib
 
+def get_icon(status):
+    icons = {
+        "Playing": "",
+        "Paused": ""
+    }
+
+    return icons.get(status, "")
+
 def on_track_change(player, e):
-    if 'xesam:artist' in e.keys() and 'xesam:title' in e.keys():
-        print('{artist} - {title}'.format(artist=e['xesam:artist'][0], title=e['xesam:title']))
+    if 'xesam:artist' in e.keys() and 'xesam:title' in e.keys(): 
+        print('{status} {artist} - {title}'.format(
+            status=get_icon(player.get_property('status')),
+            artist=e['xesam:artist'][0],
+            title=e['xesam:title']
+        ))
 
 # Get player instance
 player = Playerctl.Player(player_name='spotify')
@@ -12,7 +24,9 @@ player = Playerctl.Player(player_name='spotify')
 # Initial track information
 artist = player.get_artist()
 title = player.get_title()
-print('%s - %s' % (artist,title))
+status = get_icon(player.get_property('status'))
+
+print('%s %s - %s' % (status, artist,title))
 
 # Listener for track changes
 player.on('metadata', on_track_change)
