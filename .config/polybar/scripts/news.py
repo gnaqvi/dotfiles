1e6cc85525336d8fa645861a09e93221ff4a657b
Ghazanfar Naqvi <1874327+gnaqvi@users.noreply.github.com>
2019-05-22 17:05:39 -0400

    Update configs. Minor fixes

--- .config/polybar/scripts/news.py
+++ .config/polybar/scripts/news.py
@@ -5,7 +5,7 @@ from subprocess import call
 import re
 import textwrap
 
-nwz = feedparser.parse("https://hnrss.org/frontpage")
+nwz = feedparser.parse("https://news.ycombinator.com/rss")
 
 for f in range(0, 1):
     print(nwz.entries[f].title)
