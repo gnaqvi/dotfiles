60ee5c8dd28fbc95aeb33848c1e7e9aa1c9696e0
Ghazanfar Naqvi <1874327+gnaqvi@users.noreply.github.com>
2023-11-09 00:54:13 -0500

    Low Roar - Don't be so serious

--- .config/polybar/scripts/news/scroll_current_article.sh
+++ .config/polybar/scripts/news/scroll_current_article.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 zscroll -l 80 \
-        --delay 0.2 \
+        --delay 0.1 \
         --match-command "$(dirname "$0")/print_current_article.sh" \
         --update-check true "$(dirname "$0")/print_current_article.sh" &
 wait
